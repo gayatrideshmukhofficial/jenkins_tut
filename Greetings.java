35af75c1b210c2752354d08b0653b5d4512edc11
gayatrideshmukhofficial <gayatrideshmukhofficial@gmail.com>
2025-02-17 11:37:18 +0530

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,6 @@
+public class Greeting {
+    public static void main(String[] args) {
+        String name = "John";  // You can change the name here
+        System.out.println("Hello, " + name + "!");
+    }
+}
